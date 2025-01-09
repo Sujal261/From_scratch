ccf27c85961c3539331a58fb061e67013934611a
Sujal <sujalgyawali4@gmail.com>
2025-01-09 15:48:33 +0545

    adding softmax

--- Actication.py
+++ Actication.py
@@ -38,6 +38,36 @@ class ReLU:
               self.input.backward(relu_grad)
            
         return Tensor(output, requires_grad=requires_grad, grad_fn=grad_fn if requires_grad else None)
+    
+class Softmax:
+    def __init__(self):
+        self.input = None 
+        self.output = None 
+        
+    def __call__(self, x):
+        self.input = x
+        x_data = x.data - np.max(x.data, axis = 1, keepdims=True)
+        self.output = np.exp(x_data)/np.sum(np.exp(x_data, axis = 1, keepdims = True))
+        requires_grad = x.requires_grad
+        
+        def grad_fn(grad):
+           if self.input.requires_grad:
+               s= self.output
+               c = len(s)
+               softmax_grad = np.zeros_like(s)
+               
+               for i  in range(c):
+                   for j in range(c):
+                       if i == j:
+                           softmax_grad[i]+=softmax_grad[j]*s[i]*(1-s[i])
+                       else:
+                           softmax_grad[i]+=softmax_grad[j]*(-s[i]*s[j])
+               return softmax_grad
+                
+                           
+        return Tensor(self.output, requires_grad=requires_grad, grad_fn=grad_fn if requires_grad else None)
+        
+
 
         
 # class Tanh:
