fda16f7fe26de9cedede7b5898509f791f16e15b
Sujal <sujalgyawali4@gmail.com>
2025-01-13 07:57:52 +0545

    debugging and failing

--- layers.py
+++ layers.py
@@ -95,43 +95,107 @@ class Conv2D:
         self.in_features = in_features
         self.out_features = out_features
         self.kernel_size = kernel_size
+        
         self.padding = padding
-        self.filters = np.random.randn(out_features, in_features, kernel_size, kernel_size)
-        self.bias = np.zeros(out_features)
+        self.stride = stride
+        a, b = self.kernel_size[0], self.kernel_size[1]
+        self.filters = Tensor(np.random.randn(out_features, in_features, a, b)*scale, requires_grad = True)
+        self.bias = Tensor(np.zeros(out_features), requires_grad=True)
         
     def __call__(self, x):
-        if self.padding>0:
-            x = np.pad(x,((self.padding, self.padding),(self.padding, self.padding)),mode = 'constant', constant_values =0 )
-        i,j = x.shape 
-        k,l = self.kernel_size
+        self.x = x
+        a, b = self.kernel_size[0], self.kernel_size[1]
+        f, c, h, w = self.filters.data.shape
+        batch_size = x.data.shape[0]
+       
+        if self.padding > 0:
+            x_padded = np.pad(x.data, ((0, 0), (0, 0), (self.padding, self.padding), (self.padding, self.padding)), mode='constant')
+                   
+        else:
+            x_padded = x.data
         
-        output_height = (i-k)//self.stride+1
-        output_width = (j-l)//self.sride+1
+        padded_height , padded_width = x_padded.shape[2], x_padded.shape[3]
         
-        output = np.zeros((output_height, output_width, self.out_feature))
+        output_height = (padded_height - h) // self.stride + 1
+        output_width = (padded_width- w) // self.stride + 1
         
-        for h in range(0, output_width):
-            for w in range(output_width):
-                h_start = h*self.stride
-                h_end = h_start+k
-                w_start = w*self.stride
-                w_end = w_start + l
-                region = x[h_start:h_end, w_start:w_end]
-                
-                for f in range(self.out_features):
-                    output[h, w,f] = np.sum(region * self.filters[f, 0])+self.bias
+        
+        
+        output = np.zeros((x.data.shape[0], f, output_height, output_width))
+        # print(output )
+        for batch_idx in range(batch_size):
+            for h in range(output_height):
+                for w in range(output_width):
                     
+                        h_start = h*self.stride
+                        w_start = w*self.stride
+                        
+                        window = x_padded[batch_idx,:,h_start:h_start+a, w_start:w_start+b]
+                    
+                        for filter_idx in range(f):
+                            output[batch_idx, filter_idx, h, w]= np.sum(window * self.filters.data[filter_idx]) + self.bias.data[filter_idx]
+                            
+        requires_grad = self.filters.requires_grad or self.bias.requires_grad or x.requires_grad
                     
         def grad_fn(grad):
+            print(grad.shape)
             if self.filters.requires_grad:
-                return
-            if self.bias.grad:
-                self.bias.grad  = np.sum(grad)
-            # if x.requires_grad
+                filter_grad = np.zeros_like(self.filters.data)
+                print(filter_grad.shape)
+                for batch_idx in  range(batch_size):
+                    for h in range(output_height):
+                        for w in range(output_width):
+                            h_start = h*self.stride
+                            w_start = w*self.stride
+                            window=self.x.data[batch_idx,:,h_start:h_start+a, w_start:w_start+b]
+                            if ((h_start+a)>self.x.data[2] or (w_start+b)>self.x.data[3]):
+                                continue
+                            for filter_idx in range(f):
+                                grad_value = (grad[batch_idx, filter_idx,h,w])
+                                print(grad_value)
+                                print(filter_grad[filter_idx].shape)
+                                print(window.shape)
+                                filter_grad[filter_idx]+=window*grad_value
+                    self.filters.grad = filter_grad
+            if self.bias.requires_grad:
+                self.bias.grad = np.sum(grad, axis =(0,2, 3))
+            if x.requires_grad:
+                input_grad = np.zeros_like(self.x.data)
+                for batch_idx in range(batch_size):
+                    for h in range(output_height):
+                        for w in range(output_width):
+                            h_start = h *self.stride
+                            w_start = w*self.stride
+                            window = self.x.data[batch_idx,:,h_start:h_start+a, w_start :w_start+b]
+                            
+                            for filter_idx in range(f):
+                                grad_value = float(grad[batch_idx,filter_idx,h,w])
+                              
+                                input_grad[batch_idx,:,h_start:h_start+a, w_start:w_start+b] +=self.filters.data[filter_idx]*grad_value 
+                    self.x.backward(input_grad)
                 
+        return Tensor(output, requires_grad = requires_grad, grad_fn=grad_fn if requires_grad else None)
+    
+    
+    def parameters(self):           
+                            
+        return[self.filters, self.bias]
                 
             
-                
-                
-    
-    
+# Test example
+# Test case with matching dimensions
+batch_size = 1
+in_channels = 1  # Should match the second dimension of filters
+height = 7
+width = 7
+out_channels = 2
+kernel_size = (3, 3)
+stride = 1
+padding = 1
+
+x = Tensor(np.random.randn(batch_size, in_channels, height, width))
+conv = Conv2D(in_channels, out_channels, stride, kernel_size, padding)
+output = conv(x)
+print(output.data.shape)
+grad = np.ones_like(output.data)
+output.backward(grad)
